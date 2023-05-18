2ffcb8ab92063a81a7fa878838d8fca62bd83811
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-05-18 19:14:53 -0400

    aula 2 - nodeJS

--- node/index.js
+++ node/index.js
@@ -1,12 +1,20 @@
-const http = require("http");
-const fs = require("fs")
-require("dotenv").config();
+// const http = require("http");
+// const fs = require("fs")
+// require("dotenv").config();
+
+import http from "http";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = process.env.PORT || 3333;
-const folder = process.argv[2];
+const folder = process.argv[2]; 
 
 const server = http.createServer((req,res) => {
+   if()
    fs.readdir(folder,(err,files) => {
+      console.log("folder -> ",folder)
+
       if(err){
          console.log(err);
       }
---
+++ nodeJS/utils.js
@@ -0,0 +1,12 @@
+export function createLink(filename){
+   return `<a href=/${filename}</a><br>\n`
+}
+
+export function createVoltar(){
+
+}
+
+// module.export = {
+//    createLink,
+//    createVoltar,
+// }
